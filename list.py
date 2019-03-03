7ff3c8a5e13be29bfef5d73bb1f594bf223be9f3
wcfrank <wangchao0519@hotmail.com>
2019-03-03 22:58:57 +0800

    list

--- list.py
+++ list.py
@@ -1,6 +1,9 @@
 list1 = [1,1,2,2,2,2,3,3,3,4]
 list2 = [4,5,6,7,7,8,9,0,0,0]
 
-list1.count(2) # count the number of element 2, print 4
 
-[a*b for a,b in zip(list1,list2)] # compute the multiplication of two lists by elements
+# count the number of element 2, print 4
+list1.count(2) 
+
+# compute the multiplication of two lists by elements
+[a*b for a,b in zip(list1,list2)] 
